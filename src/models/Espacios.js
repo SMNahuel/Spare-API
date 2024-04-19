b3568c00737649d4ef954cab38696976215da9cd
Nahuelsan <nahuelsan96@gmail.com>
2024-04-19 11:17:17 -0300

    add relation

--- src/models/Espacios.js
+++ src/models/Espacios.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("space", {
+  sequelize.define("Space", {
     name: {
       type: DataTypes.STRING,
     },
--- src/models/Fotos.js
+++ src/models/Fotos.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("photo", {
+  sequelize.define("Photo", {
     url: {
       type: DataTypes.STRING,
     },
